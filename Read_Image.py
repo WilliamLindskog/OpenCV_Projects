790eadc5f29a67217ab00da167a579313c12990e
WilliamLindskog <williamlindskog@outlook.com>
2020-11-10 19:50:27 +0100

    Read Image of Ronaldo

--- Read_Image.py
+++ Read_Image.py
@@ -1,4 +1,12 @@
 # Import relevant libraries
 import cv2 as cv
 
-img = cv
+# Print an image, remove 0 so that it is not gray-scale
+img = cv.imread("Ronaldo-1.jpg", 0)
+print(img)
+
+cv.imshow("Image", img)
+
+# Showing a pic of Ronaldo, click any key for it to go away
+cv.waitKey()
+cv.destroyAllWindows()
