cb2cc2e68a4202c5a5b9e67a234e52eabb871fad
WilliamLindskog <williamlindskog@outlook.com>
2020-11-14 16:08:31 +0100

    Text on video

---
+++ Date_&_Time_on_a_Video.py
@@ -0,0 +1,18 @@
+import cv2 as cv
+import datetime
+
+cap = cv.VideoCapture(0)
+while(cap.isOpened()):
+    ret, frame = cap.read()
+
+    if ret == True:
+        font = cv.FONT_HERSHEY_SIMPLEX
+        text = "Width: " + str(cap.get(3)) + "Height: " + str(cap.get(4))
+        FRAME = cv.putText(frame, text, (10, 50), font, 1, (0, 255, 255), 2, cv.LINE_AA)
+        cv.imshow("Frame", frame)
+
+        if cv.waitKey(1) & 0xFF == ord("e"):
+            break
+
+cap.release()
+cv.destroyAllWindows()
