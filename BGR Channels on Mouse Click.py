85a7949234c268d9228f0be1b884fb663c9fff29
WilliamLindskog <williamlindskog@outlook.com>
2020-11-15 16:46:44 +0100

    Mouse click for colors

---
+++ BGR Channels on Mouse Click.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2 as cv
+
+def click_event(event, x, y, flags, param):
+    if event == cv.EVENT_LBUTTONDOWN:
+        Blue = img[y, x, 0]
+        Green = img[y, x, 1]
+        Red = img[y, x, 2]
+        font = cv.FONT_HERSHEY_SIMPLEX
+        strBGR = str(Blue) + "," + str(Green) + "," + str(Red)
+        cv.putText(img, strBGR, (x,y), font, 1, (0, 255, 255), 1)
+        cv.imshow("Image", img)
+
+img = np.zeros((500, 500, 3), np.uint8)
+#img = cv.imread("Ronaldo-Kick.jpg")
+cv.imshow("Image", img)
+
+cv.setMouseCallback("Image", click_event)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
